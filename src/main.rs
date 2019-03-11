0b3f3c6027f09729cd2798a3c114345c2967c56e
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2019-03-11 14:44:09 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::env;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		println!("Usage: halfcap 'string'");
+		return;
+	}
+	let input = &args[1];
+	let mut buf = String::with_capacity(input.len());
+	let mut up = false;
+	for c in input.chars(){
+		if c.is_alphabetic(){
+			let c = if up{
+				c.to_uppercase().collect::<Vec<_>>()
+			}else{
+				c.to_lowercase().collect::<Vec<_>>()
+			};
+			for c in c{
+				buf.push(c);
+			}
+			up = !up;
+		}else{
+			buf.push(c);
+		}
+	}
+    println!("Result: {}",buf);
+}
