03537051c4ec2ef2046697eb100d2a1d8e3ff919
Joël Lupien (Jojolepro) <jojolepro@jojolepro.com>
2020-05-27 08:14:12 -0400

    code update

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 use std::env;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	if args.len() < 2 {
-		println!("Usage: fuckthecase 'string'");
-		return;
-	}
-	let input = &args[1];
-	let mut buf = String::with_capacity(input.len());
-	let mut up = false;
-	for c in input.chars(){
-		if c.is_alphabetic(){
-			let c = if up{
-				c.to_uppercase().collect::<Vec<_>>()
-			}else{
-				c.to_lowercase().collect::<Vec<_>>()
-			};
-			for c in c{
-				buf.push(c);
-			}
-			up = !up;
-		}else{
-			buf.push(c);
-		}
-	}
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: halfcaps 'string'");
+        return;
+    }
+    let inputs = &args[1..];
+    let str_len: usize = inputs.iter().map(|s| s.len()).sum();
+    let mut buf = String::with_capacity(str_len + inputs.len());
+    let mut up = false;
+    for input in inputs {
+        for c in input.chars(){
+            if c.is_alphabetic(){
+                let c = if up{
+                    c.to_uppercase().to_string()
+                }else{
+                    c.to_lowercase().to_string()
+                };
+                buf.push_str(&c);
+                up = !up;
+            }else{
+                buf.push(c);
+            }
+        }
+        buf.push(' ');
+    }
     println!("{}",buf);
 }
